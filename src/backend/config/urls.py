9891b2f75f70789ff2464a72b560ae6ca3824fe0
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-18 23:07:55 +0200

    init

---
+++ src/backend/config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/core', include('core.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('<path:resource>', TemplateView.as_view(template_name='index.html'), name='index'),
+]
---
+++ src/backend/manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import environ
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+
+
+def main():
+    env = environ.Env()
+    env.read_env(str(PROJECT_ROOT / ".env"))
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'config.settings.{env.str("VERSION")}')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
