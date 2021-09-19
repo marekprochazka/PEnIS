3b77f2d4642c6c22ec5980bedb5aae86fc84d7aa
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-19 16:53:14 +0200

    store

--- src/frontend/src/store/index.ts
+++ src/frontend/src/store/index.ts
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from './_state'
+import mutations from './_mutations'
+import actions from './_actions'
+import getters from './_getters'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state,
+    mutations,
+    actions,
+    getters
 })
