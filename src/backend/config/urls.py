670c54a4405a1bf078180f32c47fc835785e4718
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-21 14:08:13 +0200

    added swagger

--- src/backend/config/urls.py
+++ src/backend/config/urls.py
@@ -11,7 +11,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Backend API for Flat Electronic Information System (PEnIS)",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email=""),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
