8924de6f25daa7ee1e0aefae364e48688cc626c9
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-25 17:35:33 +0200

    calendar todo

---
+++ src/frontend/src/components/Sleepover-list/Sleepover-requests-calendar.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="sleepoverRequestsCalendar__container">
+  <v-calendar
+   ref="calendar"
+   v-model="value"
+   :weekdays="weekday"
+   locale="cs-CZ"
+
+  >
+
+  </v-calendar>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Sleepover-request-calendar',
+  data() {
+    return {
+      value:"",
+      weekday: [0,1,2,3,4,5,6]
+    }
+  }
+
+}
+</script>
--- src/frontend/src/views/Sleepovers-list/App.vue
+++ src/frontend/src/views/Sleepovers-list/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="sleepoversList__container">
     <SleepoverRequestsTable />
+    <SleepoverRequestsCalendar class="mt-5" />
   </div>
 </template>
 
 <script>
 
 import SleepoverRequestsTable from '@/components/Sleepover-list/Sleepover-requests-table'
+import SleepoverRequestsCalendar from '@/components/Sleepover-list/Sleepover-requests-calendar'
 
 export default {
   name: 'Sleepovers-list',
   components: {
-    SleepoverRequestsTable
+    SleepoverRequestsTable,
+    SleepoverRequestsCalendar
   }
 }
 
