ba8f1f253fd5d6192ee9769574579fd502fda09e
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-28 17:34:17 +0200

    calendar fe setups

--- src/frontend/src/components/Sleepover-list/Sleepover-requests-calendar.vue
+++ src/frontend/src/components/Sleepover-list/Sleepover-requests-calendar.vue
@@ -1,15 +1,17 @@
 <template>
-<div class="sleepoverRequestsCalendar__container">
-  <v-calendar
-   ref="calendar"
-   v-model="value"
-   :weekdays="weekday"
-   locale="cs-CZ"
+  <div class="sleepoverRequestsCalendar__container mt-15">
+    <v-calendar
+        ref="calendar"
+        :weekdays="weekdays"
+        :type="type"
+        locale="cs-CZ"
+        :start="start"
+        :events="events"
+        event-overlap-mode="column"
+    >
 
-  >
-
-  </v-calendar>
-</div>
+    </v-calendar>
+  </div>
 </template>
 
 <script>
@@ -18,10 +20,38 @@ export default {
   name: 'Sleepover-request-calendar',
   data() {
     return {
-      value:"",
-      weekday: [0,1,2,3,4,5,6]
+      start: '2020-01-01',
+      weekdays: [1, 2, 3, 4, 5, 6, 0],
+      type: 'month',
+      events: [
+        {
+          name: 'Test title',
+          start: '2020-01-01 13:30',
+          end: '2020-01-04 13:30',
+          color: 'pink'
+        },
+          {
+          name: 'Test title',
+          start: '2020-01-01',
+          end: '2020-01-04',
+          color: 'green'
+        },
+        {
+          name: 'Test title',
+          start: '2020-01-05 13:30',
+          end: '2020-01-05 16:30',
+          color: 'pink'
+        },
+        {
+          name: 'Test title',
+          start: '2020-01-05 14:30',
+          end: '2020-01-05 17:30',
+          color: 'green'
+        },
+      ]
     }
-  }
+  },
+  methods: {}
 
 }
 </script>
